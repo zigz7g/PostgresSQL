696ff737270a975d76fd18419f520561ca3bba04
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-04-18 15:31:12 +0300

    Update Script4.sql

--- Script4.sql
+++ Script4.sql
@@ -1,6 +1,8 @@
 --Найти профессии из таблицы HumanResources.Employee, где
 --количество работников женского пола больше 1 (из выборки
 --исключить пустые поля).
-SELECT "JobTitle", COUNT( * ) AS GenderCount
+SELECT "JobTitle"
 FROM "HumanResources"."Employee"
-GROUP BY "JobTitle";
+WHERE "Gender" = 'F'
+GROUP BY "JobTitle" having count( * ) > 1;
+
