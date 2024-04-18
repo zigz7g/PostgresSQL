3915512c03e5b279ce2c3b9dbc44cd564a6292f1
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-04-18 15:00:12 +0300

    Update Script2.sql

--- Script2.sql
+++ Script2.sql
@@ -2,4 +2,4 @@
 --(Gender), которым 50 лет из таблицы HumanResources.Employee.
 SELECT AVG("VacationHours") AS AverageVacationHours
 FROM "HumanResources"."Employee"
-WHERE "Gender" = 'F' AND "BirthDate" >= '1972-01-01' AND "BirthDate" <= '1972-12-31';
+WHERE "Gender" = 'F' AND date_part('year', age(current_date, "BirthDate")) = '50';
