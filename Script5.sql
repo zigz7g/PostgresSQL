ca454a01dc9d2d02044c1eb84b8f061a33907f36
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-06-09 22:03:50 +0300

    Update Script5.sql

--- Script5.sql
+++ Script5.sql
@@ -1,7 +1,24 @@
 -- Вывести ProductID где минимальная цена (ListPrice) меньше 1 из таблицы Production.ProductCostHistory. 
 -- Добавить столбец с номером строки определяя его порядок в зависимости от минимальной цены.
-SELECT "ProductID", MIN("StandardCost") AS "MinListPrice",
-       ROW_NUMBER() OVER (ORDER BY MIN("StandardCost")) AS "RowNumber"
-FROM "Production"."ProductCostHistory"
-GROUP BY "ProductID"
-HAVING MIN("StandardCost") < 1;
+WITH MinPrices AS (
+    SELECT 
+        "ProductID", 
+        MIN("StandardCost") AS "MinListPrice"
+    FROM 
+        "Production"."ProductCostHistory"
+    GROUP BY 
+        "ProductID"
+    HAVING 
+        MIN("StandardCost") < 1
+)
+SELECT 
+    "ProductID", 
+    "MinListPrice",
+    (SELECT COUNT(*) 
+     FROM MinPrices mp2 
+     WHERE mp2."MinListPrice" <= mp1."MinListPrice") AS "RowNumber"
+FROM 
+    MinPrices mp1
+ORDER BY 
+    "RowNumber";
+
