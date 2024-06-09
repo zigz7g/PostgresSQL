3b7d51f1ceeb15228cb3434e17d348cfcfcb9721
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-06-09 21:53:02 +0300

    Update Script4.sql

--- Script4.sql
+++ Script4.sql
@@ -1,20 +1,24 @@
 -- 4. Выведите название первых пяти продуктов и для каждого из них - название следующего продукта (в алфавитном порядке) ("Production"."Product").
-WITH OrderedProducts AS (
+SELECT 
+    CurrentProducts."Name" AS CurrentProduct, 
+    NextProducts."Name" AS NextProduct
+FROM (
     SELECT 
         "ProductID", 
         "Name", 
         ROW_NUMBER() OVER (ORDER BY "Name") AS RowNum
     FROM 
         "Production"."Product"
-)
-SELECT 
-    p1."Name" AS CurrentProduct, 
-    p2."Name" AS NextProduct
-FROM 
-    OrderedProducts p1
-LEFT JOIN 
-    OrderedProducts p2
+) AS CurrentProducts
+LEFT JOIN (
+    SELECT 
+        "ProductID", 
+        "Name", 
+        ROW_NUMBER() OVER (ORDER BY "Name") AS RowNum
+    FROM 
+        "Production"."Product"
+) AS NextProducts
 ON 
-    p1.RowNum = p2.RowNum - 1
+    CurrentProducts.RowNum = NextProducts.RowNum - 1
 WHERE 
-    p1.RowNum <= 5;
+    CurrentProducts.RowNum <= 5;
