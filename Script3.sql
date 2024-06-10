5e5d695e418c6559e90e22c42317205da3a40139
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-06-11 02:29:05 +0300

    Update Script3.sql

--- Script3.sql
+++ Script3.sql
@@ -1,16 +1,8 @@
 -- Показать самую раннюю StartDate для каждого компонента (Таблица Production.BillOfMaterial). Показать поля BillOfMaterialID, ComponentID, StartDate.
-SELECT 
-    "BillOfMaterialsID", 
-    "ComponentID", 
-    "StartDate"
-FROM (
-    SELECT 
-        "BillOfMaterialsID", 
-        "ComponentID", 
-        MIN("StartDate") OVER (PARTITION BY "BillOfMaterialsID", "ComponentID") AS "StartDate",
-        ROW_NUMBER() OVER (PARTITION BY "BillOfMaterialsID", "ComponentID" ORDER BY "StartDate") AS "RowNum"
-    FROM 
-        "Production"."BillOfMaterials"
-) AS SubQuery
-WHERE 
-    "RowNum" = 1;
+SELECT "BillOfMaterialID", "ComponentID", "StartDate"
+FROM "Production"."BillOfMaterial" bom1
+WHERE "StartDate" = (
+    SELECT MIN(StartDate)
+    FROM "Production"."BillOfMateria"l bom2
+    WHERE bom2."ComponentID" = bom1."ComponentID"
+    ):
