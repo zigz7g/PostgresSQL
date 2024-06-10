370ab04437b4b515f0690c7e38b7d84570de4780
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-06-11 01:51:55 +0300

    Update Script10.sql

--- Script10.sql
+++ Script10.sql
@@ -1,25 +1,12 @@
 -- Пропустить 15 строк из результирующего набора, полученного в п.9.
-WITH "ExcludedIDs" AS (
-    SELECT 
-        p."BusinessEntityID"
-    FROM 
-        "Person"."Person" p
-    EXCEPT
-    SELECT 
-        e."BusinessEntityID"
-    FROM 
-        "HumanResources"."Employee" e
-),
-"NumberedRows" AS (
-    SELECT 
-        "BusinessEntityID", 
-        ROW_NUMBER() OVER (ORDER BY "BusinessEntityID") AS "RowNum"
-    FROM 
-        "ExcludedIDs"
-)
 SELECT 
-    "BusinessEntityID"
+    p."BusinessEntityID"
 FROM 
-    "NumberedRows"
-WHERE 
-    "RowNum" > 15;
+    "Person"."Person" p
+EXCEPT
+SELECT 
+    be."BusinessEntityID"
+FROM 
+    "Person"."BusinessEntity" be
+LIMIT 1000
+OFFSET 15;
