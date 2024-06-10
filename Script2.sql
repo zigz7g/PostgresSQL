960ff5dc142361db577f419633343fd91db4925b
Егор Плуталов <44480792+zigz7g@users.noreply.github.com>
2024-06-11 02:19:58 +0300

    Update Script2.sql

--- Script2.sql
+++ Script2.sql
@@ -1,15 +1,7 @@
 -- 2. Ранжирование товаров по точке повторного заказа в порядке убывания ("Production"."Product"). Примечание к ранжированию – ранг может начинаться с произвольного числа (например 2 или 5) главное – ранг не должен уменьшаться.
-SELECT 
-    "ProductID", 
-    "Name", 
-    "ReorderPoint", 
-    "Rank"
-FROM (
-    SELECT 
-        "ProductID", 
-        "Name", 
-        "ReorderPoint", 
-        ROW_NUMBER() OVER (ORDER BY "ReorderPoint" DESC) + 4 AS "Rank"
-    FROM 
-        "Production"."Product"
-) AS RankedProducts;
+SELECT "Name", "ReorderPoint"
+FROM "Production"."Product" p1
+WHERE (SELECT COUNT(*) 
+       FROM "Production"."Product" p2 
+       WHERE p2."ReorderPoint" < p1."ReorderPoint") + 2 AS Rank
+ORDER BY ReorderPoint ASC;
